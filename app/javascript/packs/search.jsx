3d4e881f0642454fbd1511b62ce9a1db970ed7c2
Thomas Mulloy <twmulloy@gmail.com>
2017-08-30 13:53:50 -0700

    Locked down npm dependencies and initial client file structure

--- app/javascript/packs/search.jsx
+++ app/javascript/packs/search.jsx
@@ -1,26 +1,11 @@
-// Run this example by adding <%= javascript_pack_tag 'search' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
-const Search = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Search.defaultProps = {
-  name: 'David'
-}
-
-Search.propTypes = {
-  name: PropTypes.string
-}
+import Search from '../app/Search/component'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Search />,
     document.body.appendChild(document.createElement('div')),
   )
 })
